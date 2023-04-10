6ccfa7d9c65a84347368e4409b37fcf7d84bab56
Corey Oordt <coreyoordt@gmail.com>
2023-04-10 08:57:55 -0500

    Improved logging output

--- bumpversion/logging.py
+++ bumpversion/logging.py
@@ -19,7 +19,11 @@ def setup_logging(verbose: int = 0) -> None:
         level=VERBOSITY.get(verbose, logging.DEBUG),
         format="%(message)s",
         datefmt="[%X]",
-        handlers=[RichHandler(rich_tracebacks=True, show_path=False, show_time=False, tracebacks_suppress=[click])],
+        handlers=[
+            RichHandler(
+                rich_tracebacks=True, show_level=False, show_path=False, show_time=False, tracebacks_suppress=[click]
+            )
+        ],
     )
     root_logger = logging.getLogger("")
     root_logger.setLevel(VERBOSITY.get(verbose, logging.DEBUG))
