e52c683678ea27b7dfc8ab269998407ece25ea9e
Corey Oordt <coreyoordt@gmail.com>
2023-12-18 09:18:40 -0600

    Fixed commented out workflow tasks !minor

--- tools/bump.sh
+++ tools/bump.sh
@@ -5,9 +5,7 @@ RELEASE_KIND=$(generate-changelog --output release-hint)
 echo "::notice::Suggested release type is: ${RELEASE_KIND}"
 PR_NUMBER=$(gh pr view --json number -q .number || echo "")
 echo "::notice::PR number is: ${PR_NUMBER}"
-REVISION=$(git describe --tags --long | awk -F- '{print $2}')
-echo "::notice::Revision is: ${REVISION}"
-export PR_NUMBER REVISION
+export PR_NUMBER
 bump-my-version bump -v $RELEASE_KIND
 python -m build
 python -m twine upload dist/*
