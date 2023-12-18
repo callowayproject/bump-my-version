5232255b82d31265452da6bab98d55f5094abcc7
Corey Oordt <coreyoordt@gmail.com>
2023-12-18 15:39:29 -0600

    Fixed automation. !wip

--- tools/create-release.sh
+++ tools/create-release.sh
@@ -16,25 +16,13 @@ function extract_version_content() {
   ' <<< "$changelog"
 }
 
-#rm -Rf dist
-#RELEASE_KIND=$(generate-changelog --output release-hint)
-#echo "::notice::Suggested release type is: ${RELEASE_KIND}"
-#PR_NUMBER=$(gh pr view --json number -q .number || echo "")
-#echo "::notice::PR number is: ${PR_NUMBER}"
-#export PR_NUMBER
-#bump-my-version bump -v $RELEASE_KIND
-#python -m build
-#python -m twine upload dist/*
-
 changelog=$(cat "CHANGELOG.md")
 TARGET_VERSION=$(bump-my-version show current_version)
-echo "TAG_NAME=$TARGET_VERSION"
 NOTES=$(extract_version_content "$changelog" "$TARGET_VERSION")
-
-
+echo "Creating a GitHub release for version ${TARGET_VERSION} with notes."
 gh release create \
   "${TARGET_VERSION}" \
   ./dist/* \
   --title "${TARGET_VERSION}" \
   --notes "${NOTES}" \
-  --draft \
+  --draft
